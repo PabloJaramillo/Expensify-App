2479afd5cc3ca6bbab90938018b6b0364843c4c2
Pablo <moisestj98@gmail.com>
2019-09-24 09:48:03 -0400

    other data5

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -6,8 +6,7 @@ const firebaseConfig = {
   databaseURL: process.env.FIREBASE_DATABASE_URL,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appid:process.env.FIREBASE_APP_ID 
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(firebaseConfig);
